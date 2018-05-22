42b162cd43e8ac0f2138f47d4591438a1709134f
Diego Torres <diegotorres50@hotmail.com>
2018-05-22 13:46:21 -0500

    agregando formula ppp

--- src/providers/calculator/calculator.ts
+++ src/providers/calculator/calculator.ts
@@ -10,7 +10,8 @@ import { Injectable } from '@angular/core';
 export class CalculatorProvider {
 
   results: any = {
-        imc: 0
+        imc: 0,
+        ppp: 0
   }
 
   constructor() {
@@ -19,9 +20,11 @@ export class CalculatorProvider {
 
   obtenerResultados(data: any) {
     console.log(data);
-    console.log('Intentando hacer el calculo: ' + (data.peso_actual / ((data.talla / 100) * (data.talla / 100))));
+    console.log('Intentando hacer el calculo...');
     this.results.imc = (data.peso_actual / ((data.talla / 100) * (data.talla / 100)));
+    this.results.ppp = (((data.peso_usual - data.peso_actual) * 100 ) / data.peso_actual);
     console.log(this.results);
+
     return (this.results);
   }
 
